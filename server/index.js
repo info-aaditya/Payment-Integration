940cac233ab90830b65cb897b194abbfd201fd5f
info-aaditya <gaadityag@gmail.com>
2023-07-13 01:59:39 +0545

    Added: Server Logic

--- server/index.js
+++ server/index.js
@@ -1,9 +1,40 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+// Middleware to parse JSON data
+app.use(express.json());
 
 // Root route handler
 app.get('/', (req, res) => {
   res.send('Hello, Server is running!');
+});
+
+// Route handler for creating payment intent and generating ephemeral keys
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const { amount } = req.body;
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: '2022-11-15' }
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
 });
 
 // Start the server and listen for incoming requests on port 3000
